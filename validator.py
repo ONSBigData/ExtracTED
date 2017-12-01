23c7666cb49d02584bbaeac9faaf0be09955fe87
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2017-12-01 11:19:33 +0000

    Merge sanitiser and validator

--- validator.py
+++ validator.py
@@ -1,9 +1,9 @@
 import os
+import re
 import pandas as pd
 from extractor import extract
-from sanitiser import sanitise
-from voluptuous import Schema, Required, All, Optional, Length, Url, Any, \
-    MultipleInvalid, Match
+from voluptuous import (Schema, Required, All, Optional, Length, Any,
+                        MultipleInvalid, Match, Coerce)
 
 
 """
@@ -23,7 +23,7 @@ from voluptuous import Schema, Required, All, Optional, Length, Url, Any, \
 # Lookups
 iso_country = pd.read_csv('./Lookups/ISO_COUNTRY.csv', dtype='str',
                       encoding='latin', keep_default_na=False)
-cpvs = pd.read_csv('./Lookups/CPV.csv', dtype='str')
+cpv = pd.read_csv('./Lookups/CPV.csv', dtype='str')
 ma = pd.read_csv('./Lookups/MA_MAIN_ACTIVITY.csv', dtype='str')
 td = pd.read_csv('./Lookups/TD_DOCUMENT_TYPE.csv', dtype='str')
 nc = pd.read_csv('./Lookups/NC_CONTRACT_NATURE.csv', dtype='str')
@@ -38,86 +38,107 @@ currencies = ['EUR', 'BGN', 'CHF', 'USD', 'HRK', 'CZK', 'DKK', 'HUF', 'SEK',
               'NOK', 'LTL', 'TRY', 'PLN', 'MKD', 'RON', 'JPY', 'ISK', 'SKK',
               'LVL', 'GBP', 'MTL', 'CYP', 'EEK']
 
+
+def to_number(s):
+    try:
+        n = re.sub(r'\s', '', s.replace(',', '.').replace('%', ''))
+        return float(n)
+    except (ValueError, TypeError):
+        return []
+
+
+def concatenate(lst):
+    return ' '.join(lst)
+
+
+def flat(lst):
+    if lst:
+        return lst[0]
+    else:
+        return lst
+
 # Sub Schemas
 value = Schema({
-    Required('CURRENCY'): All(str, Any(*currencies)),
-    Optional('VALUE'): All(float),
-    Optional('HIGH_VALUE'): All(float),
-    Optional('LOW_VALUE'): All(float),
-    Optional('VAT_PRCT'): All(float)
+    Optional('CURRENCY'): All(str, Any(*currencies)),
+    Optional(str): Any([], All(Coerce(flat), Coerce(to_number)))
 })
 
+
 contract_value = Schema({
-    Optional('COST'): All(value),
-    Optional('ESTIMATE'): All(value)
+    Optional(str): value,
+    Optional('NUMBER_OF_YEARS'): Any([], All(Coerce(flat), Coerce(int))),
+    Optional('NUMBER_OF_MONTHS'): Any([], All(Coerce(flat), Coerce(int)))
 })
 
 
 contractor = Schema({
-    Optional('OFFICIALNAME'): All(str),
-    Optional('COUNTRY'): All(str, Length(2), Any(*iso_country.Code)),
-    Optional('ADDRESS'): All(str),
-    Optional('TOWN'): All(str),
-    Optional('POSTAL_CODE'): All(str)
+    Optional(str): Any([], All(Coerce(flat), str)),
+    Optional('COUNTRY'): Any([], All(Coerce(flat), str, Length(2),
+                                     Any(*iso_country.Code)))
 })
 
-nuts = Match('^(' + '|'.join(iso_country.Code) + ')')
+match_nuts = Match('^(' + '|'.join(iso_country.Code) + ')')
 
-cpv = Match('^(' + '|'.join(cpvs.CODE) + ')')
+match_cpv = Match('^(' + '|'.join(cpv.CODE) + ')')
+
 
 # Document Schema
 schema = Schema({
-    Required('DOC_ID'): All(str),
-    Required('YEAR'): All(int),
-    Required('MONTH'): All(int),
-    Required('XML_FILE'): All(str),
-    Required('CODED_DATA'): All({
-        Required('NOTICE_DATA'): All({
-            Required('NO_DOC_OJS'): All(str),
-            Optional('ORIGINAL_NUTS'): All([nuts]),  # NUTS codes must begin
-                                                     # with valid country code
-            Optional('ORIGINAL_CPV'): All([cpv]),  # CPV codes must begin
-                                                   # with valid CPV code
-            Required('ISO_COUNTRY'): All(str, Length(2),
+    Required('DOC_ID'): str,
+    Required('CODED_DATA'): {
+        Required('NOTICE_DATA'): {
+            Required('NO_DOC_OJS'): All(Coerce(flat), str),
+            Optional('ORIGINAL_NUTS'): [All(str, match_nuts)],
+            Optional('ORIGINAL_CPV'): [All(str, match_cpv)],
+            Required('ISO_COUNTRY'): All(Coerce(flat), str, Length(2),
                                          Any(*iso_country.Code)),
-            Optional('IA_URL_GENERAL'): All(str),
-            Optional('REF_NOTICE'): All([str]),
-            Optional('VALUES_LIST'): All({
-                Optional('GLOBAL_VALUE'): All(value),
-                Optional('CONTRACTS_VALUE'): All([value])
-            })
-        }),
-        Required('CODIF_DATA'): All({  # need to validate codes options
-            Required('TD_DOCUMENT_TYPE'): All(str, Any(*td.CODE)),
-            Required('AA_AUTHORITY_TYPE'): All(str, Any(*aa.CODE)),
-            Required('NC_CONTRACT_NATURE'): All(str, Any(*nc.CODE)),
-            Required('PR_PROC'): All(str, Any(*pr.CODE)),
-            Required('RP_REGULATION'): All(str, Any(*rp.CODE)),
-            Required('TY_TYPE_BID'): All(str, Any(*ty.CODE)),
-            Required('AC_AWARD_CRIT'): All(str, Any(*ac.CODE)),
-            Optional('MA_MAIN_ACTIVITIES'): All([Any(*ma.CODE)])
-        })
-    }),
-    Required('CONTRACT'): All({
-        Required('OTH_NOT'): All(Any('YES', 'NO')),
-        Optional('CONTRACTING_AUTHORITY'): All(str),
-        Optional('CONTRACT_OBJECT'): All({
-            Optional('NUTS'): All([nuts]),
-            Optional('NUTS_EXTRA'): All(str),
-            Optional('CPV_MAIN'): All(cpv),
-            Optional('CONTRACT_COVERED_GPA'): All(str, Any('YES', 'NO')),
-            Optional('CONCLUSION_FRAMEWORK_AGREEMENT'): All(
-                str, Any('YES', 'NO')),
-            Optional('CONTRACTS_DPS'): All(str, Any('YES', 'NO')),
-            Optional('CPV_MAIN'): All(str),
-            Optional('CONTRACT_VALUE'): All(contract_value)
-        }),
-        Optional('AWARDS_OF_CONTRACT'): All([{
-            Optional('CONTRACTOR'): All(contractor),
-            Optional('CONTRACT_VALUE'): All(contract_value),
-        }])
-    })
+            Optional('IA_URL_GENERAL'): Any([], All(Coerce(flat), str)),
+            Optional('REF_NOTICE'): [str],
+            Optional('VALUES_LIST'): {
+                Optional('GLOBAL_VALUE'): value,
+                Optional('CONTRACTS_VALUE'): [value]
+            }
+        },
+        Required('CODIF_DATA'): {
+            Required('DS_DATE_DISPATCH'): All(Coerce(flat), str),
+            Required('TD_DOCUMENT_TYPE'): All(Coerce(flat), str, Any(*td.CODE)),
+            Required('AA_AUTHORITY_TYPE'): All(Coerce(flat), str, Any(*aa.CODE)),
+            Required('NC_CONTRACT_NATURE'): All(Coerce(flat), str, Any(*nc.CODE)),
+            Required('PR_PROC'): All(Coerce(flat), str, Any(*pr.CODE)),
+            Required('RP_REGULATION'): All(Coerce(flat), str, Any(*rp.CODE)),
+            Required('TY_TYPE_BID'): All(Coerce(flat), str, Any(*ty.CODE)),
+            Required('AC_AWARD_CRIT'): All(Coerce(flat), str, Any(*ac.CODE)),
+            Optional('MA_MAIN_ACTIVITIES'): [All(str, Any(*ma.CODE))]
+        }
+    },
+    Required('CONTRACT'): {
+        Required('OTH_NOT'): All(Coerce(flat), str, Any('YES', 'NO')),
+        Optional('CONTRACTING_AUTHORITY'): All(Coerce(flat), str),
+        Optional('CONTRACT_OBJECT'): {
+            Optional('NUTS'): [All(str, match_nuts)],
+            Optional('NUTS_EXTRA'): All(Coerce(concatenate), str),
+            Optional('CPV_MAIN'): Any([], All(Coerce(flat), str, match_cpv)),
+            Optional('CONTRACT_VALUE'): contract_value,
+            Optional(str): Any([], All(Coerce(flat), str, Any('YES', 'NO'))),
+        },
+        Optional('AWARDS_OF_CONTRACT'): [{
+            Optional('CONTRACTOR'): contractor,
+            Optional('CONTRACT_VALUE'): contract_value,
+        }]
+    }
 })
+
+
+def prune(node):
+    if isinstance(node, list):
+        for n in node:
+            prune(n)
+    elif isinstance(node, dict):
+        for k in list(node.keys()):
+            if node[k]:
+                prune(node[k])
+            else:
+                del node[k]
 
 if __name__ == "__main__":
 
@@ -141,9 +162,12 @@ if __name__ == "__main__":
                 # Extract data from xml file
                 file_path = os.path.join(DIR, f)
                 data = extract(file_path)
-                sanitise(data)
+                data = schema(data)
+
+                prune(data)
+                collection.append(data)
+
                 try:
-                    schema(data)
+                    data = schema(data)
                 except MultipleInvalid as e:
-                    collection.append(data)
                     print(str(e) + ' ---- file: ' + file_path)
